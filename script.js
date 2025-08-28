3f5822b7cdc725ab93acf47d74a928378a8abce6
WagnerJensen <wagnerjensen1962@gmail.com>
2025-08-28 16:34:49 -0300

    colocando regrade negocio

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 
 
+
 function sortear() {
 
-    const minimo = Math.ceil(document.querySelector(".input-minimo").value)
-    const maximo = Math.floor(document.querySelector(".input-maximo").value)
+const minimo = Math.ceil(document.querySelector(".input-minimo").value)
+const maximo = Math.floor(document.querySelector(".input-maximo").value)
+
+
+if(minimo<maximo){
 
     let numeroAleatorio = Math.floor(Math.random() * (maximo - minimo + 1)) + minimo;
-                       
-   alert (numeroAleatorio)
-
+    alert(numeroAleatorio)
 }
-
-
+    else {
+ alert("numero minimo maior que o numero maximo, favor corrigir")
+}
+}
 
 
